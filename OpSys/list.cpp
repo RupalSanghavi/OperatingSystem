c24e9033852a2dd69d1d7ebdef9cddc0487d6167
Rupal Sanghavi <rsanghavi@smu.edu>
2016-06-03 20:56:54 -0500

    AddPCB default

--- OpSys/list.cpp
+++ OpSys/list.cpp
@@ -1,7 +1,38 @@
 #include "list.h"
-
+#include "node.h"
+using namespace std;
 list::list()
 {
   head = nullptr;
   tail = nullptr;
+}
+//default, add to back of queue
+list::addPCB() //change to data?
+{
+  //error checks
+  Node obj = new Node(); //create a node with all the data
+  if(head == nullptr){
+      head = obj;
+      tail = obj;
+  }
+  else{
+      obj->left = tail;
+      tail->right = obj;
+      tail = obj;
+    }
+
+}
+//if position given
+list::addPCB(int pos){
+
+}
+
+list::deletePCB(int PID) //change to data?
+{
+
+}
+
+list::printVals()
+{
+
 }
--- OpSys/list.h
+++ OpSys/list.h
@@ -1,11 +1,17 @@
 #ifndef LIST_H
 #define LIST_H
-
+#include "node.h";
 
 class list
 {
+private:
+  Node head;
+  Node tail;
 public:
   list();
+  addPCB(Node obj); //change to data?
+  deletePCB(int PID); //change to data?
+  printVals();
 };
 
 #endif // LIST_H
